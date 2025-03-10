251d10213675ac6f73897bb8e6896bbf7507ebe7
Iryna Kurza <s30101@pjwstk.edu.pl>
2025-03-10 14:38:02 +0100

    added a function that finds max value, additionally added a class that has the implemented functions

--- APBD_01/APBD_01/Program.cs
+++ APBD_01/APBD_01/Program.cs
@@ -10,15 +10,31 @@ Console.WriteLine("I have no idea what I'm doing");
 //modification 3
 Console.WriteLine("let's see...");
 
-//task 3: calculate average
-public static double CalculateAverage(int[] numbers)
-{
-    if (numbers == null || numbers.Length == 0)
-        throw new ArgumentException("Array cannot be null or empty.");
-
-    return numbers.Average(); //C# has a built-in average function
-}
-
 //task 3: testing
 int[] arr = { 1, 2, 3, 4, 5 };
-Console.WriteLine(CalculateAverage(arr)); // Output: 3
+Console.WriteLine("calculated average: " + Calculator.CalculateAverage(arr)); //output: 3
+
+//task 4: testing
+Console.WriteLine("max: " + Calculator.FindMax(arr)); //output: 5
+
+public static class Calculator
+{
+    //task 3: calculate average
+    public static double CalculateAverage(int[] numbers)
+    {
+        if (numbers == null || numbers.Length == 0)
+            throw new ArgumentException("Array cannot be null or empty.");
+
+        return numbers.Average(); //C# has a built-in average function
+    }
+
+
+    //task 4: find max
+    public static int FindMax(int[] numbers)
+    {
+            if (numbers == null || numbers.Length == 0)
+                throw new ArgumentException("Array cannot be null or empty.");
+            return numbers.Max(); //built-in max function
+    }
+
+}
