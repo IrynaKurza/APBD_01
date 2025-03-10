5c326745cc0bb3020cd4d7217500938916b2ef48
Iryna Kurza <s30101@pjwstk.edu.pl>
2025-03-10 03:52:26 +0100

    Modification 3

--- APBD_01/APBD_01/Program.cs
+++ APBD_01/APBD_01/Program.cs
@@ -6,3 +6,6 @@ Console.WriteLine("Well, it's my first time using C#");
 
 //modification 2
 Console.WriteLine("I have no idea what I'm doing");
+
+//modification 3
+Console.WriteLine("let's see...");
